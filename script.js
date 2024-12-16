b340e60b17080a3bb6219078c91a4e33aa040256
Belzar Sirus <belzar@ymail.com>
2024-12-16 04:08:29 +0100

    Upd

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let numButtonClicks = 0;
+
+
 function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-        navigator.clipboard.readText()
-  .then(text => {
-            document.getElementById("d1").textContent = text;
-    console.log('Pasted content: ', text);
-  })
-  .catch(err => {
-    console.error('Failed to read clipboard contents: ', err);
-  });
+  numButtonClicks = numButtonClicks + 1;
+  document.getElementById("mainDiv").textContent = "Button Clicked times: " + numButtonClicks;
+
+  navigator.clipboard
+    .readText()
+    .then((text) => {
+      document.getElementById("d1").textContent = text;
+      console.log("Pasted content: ", text);
+    })
+    .catch((err) => {
+      console.error("Failed to read clipboard contents: ", err);
+    });
 }
